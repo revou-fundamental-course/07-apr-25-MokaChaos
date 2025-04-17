b543dbc1a84b31efe6db6e2dd98c12ec32dae479
MokaChaos <aliffadhlu9@gmail.com>
2025-04-17 20:16:40 +0700

    Finalizing Web content. Changing banner picture and adding the output box for the message us section.  cleaning code from unused parts. working out the responsive size part of the website.

--- js/script.js
+++ js/script.js
@@ -15,34 +15,43 @@ button.addEventListener("click", function() {
 })
 
 function showSlides(){
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}    
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
-  setTimeout(showSlides, 2000); // Change image every 2 seconds
-}
-
-let submit = document.getElementById("tbl-submit")
-submit.addEventListener("click", function() {
-    submit_validation();
-})
-
-function submit_validation(){
-    const name = document.getElementById("name").value;
-    console.log("Name:", name);
-
-    if(name == "") {
-        alert("please enter your name");
-    } else {
-        alert("form submitted successfully");
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
     }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}    
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";  
+    dots[slideIndex-1].className += " active";
+    setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
+
+// let submit = document.getElementById("tbl-submit")
+// submit.addEventListener("click", function() {
+//     submit_validation();
+// })
+
+document.getElementById('message-form').addEventListener('submit', function (e) {
+    e.preventDefault(); // prevent page reload
+  
+    const name = document.getElementById('name').value;
+    const dob = document.getElementById('dob').value;
+    const message = document.getElementById('message').value;
+    const gender = document.querySelector('input[name="jk"]:checked').value;
+  
+    const now = new Date().toString();
+  
+    const outputBox = document.getElementById('output');
+    outputBox.innerHTML = `
+      <p><strong>Current time:</strong> ${now}</p>
+      <p><strong>Name:</strong> ${name}</p>
+      <p><strong>Date of Birth:</strong> ${dob}</p>
+      <p><strong>Gender:</strong> ${gender}</p>
+      <p><strong>Message:</strong> ${message}</p>
+    `;
+  });
